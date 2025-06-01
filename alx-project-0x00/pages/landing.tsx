3ddd3a70f17403c1d9228a8bd7ac67c915c4d31a
Youssef HIMMI <yhimmi1@gmail.com>
2025-06-01 20:43:28 +0200

    feat: add Card component to landing page and demonstrate reuse

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,11 @@
+import Card from '@/components/Card';
 import React from 'react';
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
+      <Card />
     </div>
   );
 };
